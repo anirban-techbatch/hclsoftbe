be9c3e220171f94520bed5f6dc9915b53548428e
Anirban Das <anirban-das@prod.hclpnp.com>
2024-11-21 08:35:31 +0530

    commented

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
- 
-
-
 import fs from 'fs'
 import http from 'http'
 import querystring from 'querystring'
@@ -21,44 +18,56 @@ let path=req.url.split('?')[0]
             }
         case '/fixlets':
             {
+                try{
             let data= fs.readFileSync('fixlets.csv','utf-8')
             let jsondata= parseCSV(data)
             console.log(jsondata)
             res.end(JSON.stringify(jsondata));
+        }catch(err){
+            console.log(err)
+            res.end(JSON.stringify({'status':"Server error"}));
+        }
             break;
             }
         case '/fixletsfromserver':
             {
-            
+            try{
             const parsed = url.parse(req.url);
             const query  = querystring.parse(parsed.query);
             let jsondata= await getDataFromServer(query.criticality)
             console.log(jsondata)
             res.end(JSON.stringify(modifyData(jsondata)));
+        }catch(err){
+            console.log(err)
+            res.end(JSON.stringify({'status':"Server error"}));
+        }
             break;
             }
-        case '/fixletsfromserver':
-                {
-                let jsondata= await getDataFromServer(query.criticality)
-                console.log(jsondata)
-                res.end(JSON.stringify(jsondata));
-                break;
-                }
+     
        case '/fixletstats':
                  {
-
-                    let data=await getDataFromServer()
+                    try{
+                        let data=await getDataFromServer()
                     let json={
                         'CriticalityAggregate':await calculateAggrigate('criticality',data),
                         'MonthAggregate':await calculateAggrigate('monthly',data)
                     }
 
                     res.end(JSON.stringify(json));
+                    }catch(err){
+                        console.log(err)
+                        res.end(JSON.stringify({'status':"Server error"}));
+                    }
+
+                    
+
+                    
                     break;
                     }
 
 
     }
+    
 
 }).listen(8181)
 
